646d4bc55c232320294ac48651eeff9d086bb2a3
VuksanFilip <filipvuksan.iphone@gmail.com>
2022-11-14 22:14:42 +0100

    Changed Intent to UserLoginActivity

--- app/src/main/java/com/example/uberapp_tim22/SplashActivity.java
+++ app/src/main/java/com/example/uberapp_tim22/SplashActivity.java
@@ -20,7 +20,7 @@ public class SplashActivity extends AppCompatActivity {
         new Timer().schedule(new TimerTask() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashActivity.this, DetailActivity.class));
+                startActivity(new Intent(SplashActivity.this, UserLoginActivity.class));
                 finish();
             }
         }, SPLASH_TIME_OUT);
