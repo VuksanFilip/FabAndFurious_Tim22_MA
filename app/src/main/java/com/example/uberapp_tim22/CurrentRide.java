f6c419696e2989eb9893c6892231fb999533e884
AndreaSV692020 <andrea.katzenberger01@gmail.com>
2023-01-20 00:24:07 +0100

    Update file

---
+++ app/src/main/java/com/example/uberapp_tim22/CurrentRide.java
@@ -0,0 +1,14 @@
+package com.example.uberapp_tim22;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CurrentRide extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_current_ride);
+    }
+}
