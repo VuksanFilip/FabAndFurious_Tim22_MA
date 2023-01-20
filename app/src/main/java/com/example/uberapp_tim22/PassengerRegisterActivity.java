33bfdcdd2b93fb86b51183d1b6061b64b31c3e26
AndreaSV692020 <andrea.katzenberger01@gmail.com>
2023-01-20 16:00:35 +0100

    Update PassengerRegisterActivity.java

--- app/src/main/java/com/example/uberapp_tim22/PassengerRegisterActivity.java
+++ app/src/main/java/com/example/uberapp_tim22/PassengerRegisterActivity.java
@@ -11,6 +11,8 @@ import android.widget.Toast;
 
 public class PassengerRegisterActivity extends AppCompatActivity {
 
+    Button loginBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,10 +20,20 @@ public class PassengerRegisterActivity extends AppCompatActivity {
 
 
         Button registerBtn = findViewById(R.id.registerButton);
+        loginBtn = findViewById(R.id.button8);
+
         registerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(PassengerRegisterActivity.this, PassengerMainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PassengerRegisterActivity.this, UserLoginActivity.class);
                 startActivity(intent);
             }
         });
