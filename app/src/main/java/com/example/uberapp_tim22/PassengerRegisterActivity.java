869b89f34be902aed108a89727a47aceca9f38af
AndreaSV692020 <andrea.katzenberger01@gmail.com>
2023-01-20 20:22:39 +0100

    Update PassengerRegisterActivity.java

--- app/src/main/java/com/example/uberapp_tim22/PassengerRegisterActivity.java
+++ app/src/main/java/com/example/uberapp_tim22/PassengerRegisterActivity.java
@@ -1,11 +1,9 @@
 package com.example.uberapp_tim22;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -18,12 +16,9 @@ public class PassengerRegisterActivity extends AppCompatActivity {
 
 
         Button registerBtn = findViewById(R.id.registerButton);
-        registerBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(PassengerRegisterActivity.this, PassengerMainActivity.class);
-                startActivity(intent);
-            }
+        registerBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(PassengerRegisterActivity.this, PassengerMainActivity.class);
+            startActivity(intent);
         });
     }
 
