894f7eab5c539c4b3605ed935bffd2bc65a3def4
AndreaSV692020 <andrea.katzenberger01@gmail.com>
2023-01-20 12:52:53 +0100

    Update DriverMainActivity.java

--- app/src/main/java/com/example/uberapp_tim22/DriverMainActivity.java
+++ app/src/main/java/com/example/uberapp_tim22/DriverMainActivity.java
@@ -3,12 +3,17 @@ package com.example.uberapp_tim22;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class DriverMainActivity extends AppCompatActivity {
+
+    Button acceptanceRide;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,15 @@ public class DriverMainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("FAB Car");
+
+        acceptanceRide = findViewById(R.id.button);
+        acceptanceRide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DriverMainActivity.this, AcceptanceRide.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
