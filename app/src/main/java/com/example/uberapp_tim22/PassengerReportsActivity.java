15b239f333a07d2dea212bce8ba8cc5111c1530c
AndreaSV692020 <andrea.katzenberger01@gmail.com>
2023-01-22 18:18:07 +0100

    Create PassengerReportsActivity.java

---
+++ app/src/main/java/com/example/uberapp_tim22/PassengerReportsActivity.java
@@ -0,0 +1,83 @@
+package com.example.uberapp_tim22;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class PassengerReportsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passenger_reports);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("FAB Car");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.example_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if(id == R.id.item1){
+            Intent intent = new Intent(PassengerReportsActivity.this, PassengerAccountActivity.class);
+            startActivity(intent);
+        }
+        if(id == R.id.item2){
+            Intent intent = new Intent(PassengerReportsActivity.this, PassengerInboxActivity.class);
+            startActivity(intent);
+        }
+        if(id == R.id.item3){
+            Intent intent = new Intent(PassengerReportsActivity.this, PassangerRideHistory.class);
+            startActivity(intent);
+        }
+        if(id == R.id.item4){
+            Intent intent = new Intent(PassengerReportsActivity.this, PassengerMainActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "onStart()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "onResume()",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "onPause()",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "onStop()",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "onDestroy()",Toast.LENGTH_SHORT).show();
+    }
+}
