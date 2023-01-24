b7a708a66dfa5cc0f4d87bbb0d5c8520ced758c0
popovbojana <popov.bojana@gmail.com>
2023-01-24 21:46:09 +0100

    Added map

--- app/src/main/java/com/example/uberapp_tim22/PassengerMainActivity.java
+++ app/src/main/java/com/example/uberapp_tim22/PassengerMainActivity.java
@@ -1,31 +1,50 @@
 package com.example.uberapp_tim22;
 
+import static com.example.uberapp_tim22.fragments.MapFragment.MY_PERMISSIONS_REQUEST_LOCATION;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.Configuration;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import com.example.uberapp_tim22.dialogs.LocationDialog;
+import com.example.uberapp_tim22.fragments.DrawRouteFragment;
+import com.example.uberapp_tim22.fragments.MapFragment;
+import com.example.uberapp_tim22.tools.FragmentTransition;
+
+
 public class PassengerMainActivity extends AppCompatActivity {
-
-
-
+    private CharSequence mTitle;
+    private AlertDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passenger_main);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("FAB Car");
-
+        FragmentTransition.to(MapFragment.newInstance(), this, false);
         Button findDriverBtn = findViewById(R.id.button);
         Button favorites = findViewById(R.id.button3);
 
@@ -81,32 +100,72 @@ public class PassengerMainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        Toast.makeText(this, "onStart()", Toast.LENGTH_SHORT).show();
+    protected void onResume() {
+        // TODO Auto-generated method stub
+        super.onResume();
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        boolean gps = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        boolean wifi = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        Log.i("wwww", String.valueOf(gps));
+        Log.i("wqqqq", String.valueOf(wifi));
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        Toast.makeText(this, "onResume()",Toast.LENGTH_SHORT).show();
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // location-related task you need to do.
+                    if (ContextCompat.checkSelfPermission(this,
+                            android.Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(this, "LOCATION GRANTED", Toast.LENGTH_SHORT).show();
+                    }
+
+                } else if (grantResults.length > 0
+                        && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+
+                    if (ContextCompat.checkSelfPermission(this,
+                            Manifest.permission.ACCESS_COARSE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(this, "LOCATION GRANTED", Toast.LENGTH_SHORT).show();
+
+                    }
+
+                }
+            }
+
+        }
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        mTitle = title;
+        getSupportActionBar().setTitle(mTitle);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Toast.makeText(this, "onPause()",Toast.LENGTH_SHORT).show();
-    }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Toast.makeText(this, "onStop()",Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Toast.makeText(this, "onDestroy()",Toast.LENGTH_SHORT).show();
     }
 }
