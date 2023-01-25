277e289e074a288f662dfa49586cb8661fe73d6f
andreakatzenberger <andrea.katzenberger01@gmail.com>
2023-01-25 15:18:30 +0100

    Created driver's activities: account, reports, statistics

--- app/src/main/java/com/example/uberapp_tim22/DriverReportsActivity.java
+++ app/src/main/java/com/example/uberapp_tim22/DriverReportsActivity.java
@@ -1,62 +1,25 @@
 package com.example.uberapp_tim22;
 
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
-import com.google.android.material.navigation.NavigationView;
+public class DriverReportsActivity extends AppCompatActivity {
 
-public class DriverAccountActivity extends AppCompatActivity {
-
-//    DrawerLayout drawerLayout;
-//    NavigationView navigationView;
-//    Button current_ride;
-    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_driver_account);
-
-//        navigationView.findViewById(R.id.nav_view);
-//        drawerLayout.findViewById(R.id.drawer_layout);
-//        current_ride.findViewById(R.id.button11);
+        setContentView(R.layout.activity_driver_reports);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("FAB Car");
-
-        Button proba = findViewById(R.id.button9);
-        proba.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Intent intent = new Intent(DriverAccountActivity.this, DriverStatisticsActivity.class);
-                startActivity(intent);
-            }
-        });
-
-//        navigationView.bringToFront();
-//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav_drawer, R.string.close_nav_drawer);
-//        drawerLayout.addDrawerListener(toggle);
-//        toggle.syncState();
-
-//        current_ride.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(DriverAccountActivity.this, CurrentRideDriver.class);
-//                startActivity(intent);
-//            }
-//        });
     }
 
     @Override
@@ -71,23 +34,23 @@ public class DriverAccountActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         if(id == R.id.item1){
-            Intent intent = new Intent(DriverAccountActivity.this, DriverAccountActivity.class);
+            Intent intent = new Intent(DriverReportsActivity.this, DriverAccountActivity.class);
             startActivity(intent);
         }
         if(id == R.id.item2){
-            Intent intent = new Intent(DriverAccountActivity.this, DriverInboxActivity.class);
+            Intent intent = new Intent(DriverReportsActivity.this, DriverInboxActivity.class);
             startActivity(intent);
         }
         if(id == R.id.item3){
-            Intent intent = new Intent(DriverAccountActivity.this, DriverRideHistory.class);
+            Intent intent = new Intent(DriverReportsActivity.this, DriverRideHistory.class);
             startActivity(intent);
         }
         if(id == R.id.item4){
-            Intent intent = new Intent(DriverAccountActivity.this, DriverMainActivity.class);
+            Intent intent = new Intent(DriverReportsActivity.this, DriverMainActivity.class);
             startActivity(intent);
         }
         if(id == R.id.item5){
-            Intent intent = new Intent(DriverAccountActivity.this, UserLoginActivity.class);
+            Intent intent = new Intent(DriverReportsActivity.this, UserLoginActivity.class);
             startActivity(intent);
         }
         return super.onOptionsItemSelected(item);
@@ -122,4 +85,5 @@ public class DriverAccountActivity extends AppCompatActivity {
         super.onDestroy();
         Toast.makeText(this, "onDestroy()",Toast.LENGTH_SHORT).show();
     }
+
 }
