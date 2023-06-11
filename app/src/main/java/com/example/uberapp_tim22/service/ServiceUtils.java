012a10336b20f137a44a2ed8ea76e7fd027171e3
Filip Vuksan <filipvuksan.iphone@gmail.com>
2023-06-11 15:12:56 +0200

    changed ip

--- app/src/main/java/com/example/uberapp_tim22/service/ServiceUtils.java
+++ app/src/main/java/com/example/uberapp_tim22/service/ServiceUtils.java
@@ -9,9 +9,9 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ServiceUtils {
 
-    //Filip 192.168.1.17
+    //Filip 192.168.1.9
     //Andrea 192.168.0.22
-    public static final String SERVICE_API_PATH = "http://192.168.1.17:8084/api/";
+    public static final String SERVICE_API_PATH = "http://192.168.1.9:8084/api/";
     public static final String driver = "driver";
     public static final String passenger = "passenger";
     public static final String vehicle = "vehicle";
